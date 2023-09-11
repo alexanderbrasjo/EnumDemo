9fc0397918fea937e4c70b2f2d44eab1c9602d63
Alexander Brasjö <alexander.brasjo@iths.se>
2023-09-11 10:29:45 +0200

    made one change

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello Alexander!");
             Console.WriteLine("Hello again");
             Console.WriteLine("Detta är version 3");
+            Console.WriteLine("En gång från cmd");
         }
     }
 }
